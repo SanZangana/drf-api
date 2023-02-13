be71bc08f42c30e9e89535fd8911e1f9a42e8e84
SanZangana <San9@live.se>
2023-02-13 07:50:27 +0000

    Move files inside 'apis' folder

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
-    create_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=250, blank=True)
     content = models.TextField(blank=True)
