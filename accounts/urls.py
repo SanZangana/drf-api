136cdc806570700f70af7a63b2e27d6b344914b1
SanZangana <San9@live.se>
2023-02-14 05:58:51 +0000

    Add profiles detail view

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from accounts import views
 
 urlpatterns = [
     path('accounts/', views.AccountsList.as_view()),
+    path('accounts/<int:pk>/', views.AccountDetail.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Account
@@ -9,3 +11,27 @@ class AccountsList(APIView):
         accounts = Account.objects.all()
         serializer = AccountSerializer(accounts, many=True)
         return Response(serializer.data)
+
+
+class AccountDetail(APIView):
+    serializer_class = AccountSerializer
+    def get_object(self, pk):
+        try:
+            account = Account.objects.get(pk=pk)
+            return account
+        except Account.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        account = self.get_object(pk)
+        serializer = AccountSerializer(account)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        account = self.get_object(pk)
+        serializer = AccountSerializer(account, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
