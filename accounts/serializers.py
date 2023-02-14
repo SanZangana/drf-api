5bed7c313e6d8547287c3f5e6ff4284f75b33402
SanZangana <San9@live.se>
2023-02-14 06:48:15 +0000

    Add login/ logout views, IsOwner permission and is_owner field to AccountSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,9 +3,16 @@ from .models import Account
 
 class AccountSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
 
     class Meta:
         model = Account
         fields = [
-            "id", "owner", "created_at", "updated_at", "name", "content", "image"
+            "id", "owner", "created_at", "updated_at", "name", "content", "image",
+            "is_owner"
         ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,32 +4,41 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Account
 from .serializers import AccountSerializer
+from apis.permissions import IsOwnerOrReadOnly
+
 
 
 class AccountsList(APIView):
     def get(self, request):
         accounts = Account.objects.all()
-        serializer = AccountSerializer(accounts, many=True)
+        serializer = AccountSerializer(accounts, many=True,
+        context={'request': request})
         return Response(serializer.data)
 
 
 class AccountDetail(APIView):
     serializer_class = AccountSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
     def get_object(self, pk):
         try:
             account = Account.objects.get(pk=pk)
+            self.check_object_permissions(self.request, account)
             return account
         except Account.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         account = self.get_object(pk)
-        serializer = AccountSerializer(account)
+        serializer = AccountSerializer(
+            account, context={'request': request}
+            )
         return Response(serializer.data)
 
     def put(self, request, pk):
         account = self.get_object(pk)
-        serializer = AccountSerializer(account, data=request.data)
+        serializer = AccountSerializer(account, data=request.data, context={'request': request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
