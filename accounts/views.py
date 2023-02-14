2e56ff9cfbf9adc3679c1b80da36c5c924e4627d
SanZangana <San9@live.se>
2023-02-14 05:31:43 +0000

    Import and add serializer

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Account
+from .serializers import AccountSerializer
 
-# Create your views here.
+
+class AccountsList(APIView):
+    def get(self, request):
+        accounts = Account.objects.all()
+        serializer = AccountSerializer(accounts, many=True)
+        return Response(serializer.data)
