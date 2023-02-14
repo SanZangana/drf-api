abdf481e666993a5b9e913f2f91c57b680ff68f0
SanZangana <San9@live.se>
2023-02-14 09:27:13 +0000

    Add url to path in urls.py in api directory

--- apis/urls.py
+++ apis/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('accounts.urls')),
+    path('', include('posts.urls')),
 
 ]
