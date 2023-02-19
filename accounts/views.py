9cecb4e16caf3aa8f84db1fcfda3050254b66404
SanZangana <San9@live.se>
2023-02-19 06:02:41 +0000

    Add more model serializers to Class Meta for accounts and posts

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from apis.permissions import IsOwnerOrReadOnly
 from .models import Account
 from .serializers import AccountSerializer
@@ -9,8 +11,26 @@ class AccountList(generics.ListAPIView):
     List all profiles.
     No create view as profile creation is handled by django signals.
     """
-    queryset = Account.objects.all()
+    queryset = Account.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('created_at')
     serializer_class = AccountSerializer
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__following__followed__account',
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
 class AccountDetail(generics.RetrieveUpdateAPIView):
@@ -18,5 +38,9 @@ class AccountDetail(generics.RetrieveUpdateAPIView):
     Retrieve or update a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Account.objects.all()
+    queryset = Account.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('created_at')
     serializer_class = AccountSerializer
