35a638788bc4eb0c2ef258c4b88c7c37470bd481
SanZangana <San9@live.se>
2023-02-19 06:32:33 +0000

    Add blank line

--- comments/views.py
+++ comments/views.py
@@ -20,4 +20,3 @@ class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
-    
