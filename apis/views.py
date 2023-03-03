0ba2cd03cbb4fd012387cc81e9fd8eba33b45fce
SanZangana <San9@live.se>
2023-03-03 06:50:08 +0000

    Add message for response

--- apis/views.py
+++ apis/views.py
@@ -4,5 +4,5 @@ from rest_framework.response import Response
 @api_view()
 def root_route(request):
     return Response({
-        "message": "Welcome to my API!"
+        "message": "Welcome to my API for caption-this!!"
     })
